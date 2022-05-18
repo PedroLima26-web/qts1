1294ae088fc5070006d92b3f06d92425677430a5
PedroLima26-web <69848820+PedroLima26-web@users.noreply.github.com>
2022-05-18 12:39:56 -0300

    Add files via upload

---
+++ Revisão/Jaca.js
@@ -0,0 +1,15 @@
+function tabela () {
+    document.location.href="tabela.html"
+}
+
+function how () {
+   document.location.href="quem.html"
+}
+
+function mundos () {
+document.location.href="mundos.html"
+}
+
+function volta () {
+    document.location.href="Main.html";
+}
